f6a12050894635eee6c54a786b38b3c0970fcf10
Dat T. Tran <datttrian@gmail.com>
2023-08-19 22:38:22 -0400

    LinkedList

---
+++ Start/Ch2_Basic/LinkedList/Program.cs
@@ -0,0 +1,38 @@
+﻿// Define some strings that represent song names
+string[] songs = {"Shout", "Satisfaction", "Help!",
+                            "Stairway to Heaven", "Come Sail Away",
+                            "All Night Long", "Right Here Right Now"};
+
+// TODO: Create a LinkedList that holds strings
+LinkedList<String> mylist = new(songs);
+
+// TODO: Items can be added at the front or back of the list
+mylist.AddFirst("African");
+mylist.AddLast("The Twist");
+
+// TODO: Like other collections, a LinkedList can be iterated
+foreach (string s in mylist)
+{
+    Console.WriteLine(s);
+}
+
+// TODO: First and Last properties return LinkedListNodes
+LinkedListNode<string> first = mylist.First;
+LinkedListNode<string> last = mylist.Last;
+Console.WriteLine(first);
+Console.WriteLine(last);
+
+// TODO: Items can be added or removed relative to an existing item
+mylist.AddAfter(first, "Here comes the sun");
+foreach (string s in mylist)
+{
+    Console.WriteLine(s);
+}
+
+// TODO: Search for items in the list with Contains and Find
+Console.WriteLine(mylist.Contains("Satisfaction"));
+Console.WriteLine(mylist.Find("Help!").Value);
+
+// TODO: The list can then be traversed with those properties
+Console.WriteLine(first.Next.Value);
+Console.WriteLine(last.Previous.Value);
